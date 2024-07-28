9e24c93835c6b739a7e3374fe5b035bc21a7914a
cindyyzhu <cindyzhu8207@gmail.com>
2024-07-28 12:10:52 -0400

    buttons

--- team-up/src/app/embed/MapEmbeded.js
+++ team-up/src/app/embed/MapEmbeded.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const MapEmbed = () => {
   return (
-    <div style={{ width: '100%', maxWidth: '640px', margin: '0 auto' }}>
+    <div style={{ width: '100%', maxWidth: '800px', margin: '0 auto' }}>
       <iframe
         src="https://www.google.com/maps/d/u/0/embed?mid=1r2i0Pl-sbfuXsJ1K2-9iJZC8OOPMrsM&ehbc=2E312F&noprof=1"
-        width="640"
-        height="480"
+        width="800"
+        height="550"
         style={{ border: '0' }}
         allowFullScreen
         loading="lazy"
