5a00d76e138dd55ef1b1590602c248bcda64c35c
cindyyzhu <cindyzhu8207@gmail.com>
2024-07-28 10:11:54 -0400

    embeded calendar and map

---
+++ team-up/src/app/calendar/page.js
@@ -0,0 +1,13 @@
+"use client"
+
+import CalendarEmbeded from './CalendarEmbeded.js';
+
+const HomePage = () => {
+  return (
+    <div>
+      <CalendarEmbeded />
+    </div>
+  );
+};
+
+export default HomePage;
